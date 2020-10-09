3eb7fd499cd3a190201d10e59f64ba9bb18ef1bc
wolfie <1039719274@qq.com>
2020-10-09 10:59:57 +0800

    '添加logo'

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import './index.less'
 import {Route,Switch,Redirect} from 'react-router-dom'
 import {adminRouter} from "./routes";
-
+import Frame from "./components/Frame";
 
 import {
     Button
@@ -12,19 +12,20 @@ class App extends Component {
     render() {
         return (
             <div>
-                <div>这里是公共的部分</div>
-                <Switch>
-                {
-                    adminRouter.map((item, index) => {
-                        return <Route key={item.pathname+index} path={item.pathname} exact={item.exact} render={(routerProps) => {
-                            //TODO 页面级别的权限验证
-                            return <item.component {...routerProps}/>
-                        }}/>
-                    })
-                }
-                <Redirect from={'/admin'} to={adminRouter[0].pathname} exact/>
-                <Redirect to={'/404'} />
-                </Switch>
+                <Frame>
+                    <Switch>
+                        {
+                            adminRouter.map((item, index) => {
+                                return <Route key={item.pathname+index} path={item.pathname} exact={item.exact} render={(routerProps) => {
+                                    //TODO 页面级别的权限验证
+                                    return <item.component {...routerProps}/>
+                                }}/>
+                            })
+                        }
+                        <Redirect from={'/admin'} to={adminRouter[0].pathname} exact/>
+                        <Redirect to={'/404'} />
+                    </Switch>
+                </Frame>
             </div>
         );
     }
