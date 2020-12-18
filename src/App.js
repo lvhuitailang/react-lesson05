9e61a1d2710c03152820691a32004be05147847d
wolfie <1039719274@qq.com>
2020-12-18 11:18:37 +0800

    'first'

--- src/App.js
+++ src/App.js
@@ -1,30 +1,11 @@
 import React, {Component} from 'react';
 import './index.less'
 
-import {
-    Button
-} from "antd";
-
-const testHoc = (WrappedComponent)=>{
-    return class HocComponent extends Component{
-        render() {
-            return (
-                <>
-                    <WrappedComponent/>
-                    <div>这是高阶组件</div>
-                </>
-            )
-        }
-    }
-}
-
-@testHoc
 class App extends Component {
     render() {
         return (
             <div>
-                App
-                <Button >按钮</Button>
+              测试
             </div>
         );
     }
